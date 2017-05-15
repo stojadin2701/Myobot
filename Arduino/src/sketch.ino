a1a7e8b3c8122e57e584c278068c89aa740d41d8
Stojadin2701 <stojadin2701@gmail.com>
2017-05-15 16:31:20 +0000

    Basic communication protocol

--- Arduino/src/sketch.ino
+++ Arduino/src/sketch.ino
@@ -1,13 +1,76 @@
+
+//Pins for enabling motor control
+const unsigned char ENABLE_PINS[] = {7, 8};
+
+const unsigned char MOTOR_PINS[] = {5, 6, 9, 10};
+
+const unsigned char DISTANCE_TRIG_PIN = 3;
+
+const unsigned char DISTANCE_ECHO_PIN = 4;
+
+enum Command { REQUEST_DISTANCE = 1, SET_MOTORS };
+
+long microsecondsToCentimeters(long duration){
+	return duration/29/2;
+}
+
+int read_distance(){
+	long duration;
+	pinMode(DISTANCE_TRIG_PIN, OUTPUT);
+	digitalWrite(DISTANCE_TRIG_PIN, LOW);
+	delayMicroseconds(2);
+	digitalWrite(DISTANCE_TRIG_PIN, HIGH);
+	delayMicroseconds(10);
+
+	digitalWrite(DISTANCE_TRIG_PIN, LOW);
+
+	pinMode(DISTANCE_ECHO_PIN, INPUT);
+	duration = pulseIn(DISTANCE_ECHO_PIN, HIGH);
+
+	return microsecondsToCentimeters(duration);
+}
+
 void setup() {
+	for (unsigned char i = 0; i < sizeof(ENABLE_PINS)/sizeof(unsigned char); i++){
+		pinMode(ENABLE_PINS[i], OUTPUT);
+		digitalWrite(ENABLE_PINS[i], HIGH);
+	}
+
+	for(unsigned char i = 0; i < sizeof(MOTOR_PINS)/sizeof(unsigned char); i++){
+		pinMode(MOTOR_PINS[i], OUTPUT);
+	}
+	
+	pinMode(DISTANCE_TRIG_PIN, OUTPUT);
+	pinMode(DISTANCE_ECHO_PIN, INPUT);
+
 	Serial.begin(9600); // set the baud rate
 	Serial.println("Ready"); // print "Ready" once
 }
 
 void loop() {
-	char inByte = ' ';
+/*	unsigned char received_command = 0;
 	if(Serial.available()){ // only send data back if data has been sent
-		char inByte = Serial.read(); // read the incoming data
-		Serial.println(inByte); // send the data back in a new line so that it is not all one long line
+		received_command = Serial.read(); // read the incoming data
+		switch(received_command){
+			case REQUEST_DISTANCE: {
+				Serial.println("DISTANCE REQUESTED");
+				Serial.println(read_distance());				
+				break;
+			}
+			case SET_MOTORS: {
+				Serial.println("SETTING MOTOR POWERS");
+				break;
+			}
+			default: {
+				Serial.println("COMMAND UNKNOWN");
+				break;
+			}
+		}
+		//Serial.println(inByte); // send the data back in a new line so that it is not all one long line
 	}
 	delay(100); // delay for 1/10 of a second
+*/
+	Serial.print(read_distance());
+	delay(100);
+
 }
--- Pi/main.py
+++ Pi/main.py
@@ -3,13 +3,13 @@ import time
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
-counter = 32
+counter = 0
 
 while True:
-	counter+=1
+	counter=1
 	ser.write(str(chr(counter)))
 	print ser.readline()
 	time.sleep(.1)
-	if(counter == 255):
-		counter=32
+	if(counter == 3):
+		counter=0
 
