ee0bfe236c2ede97bb3a6b163a23a239757a85f6
Stojadin2701 <stojadin2701@gmail.com>
2017-05-18 12:04:37 +0000

    Multiple distance sensor sampling

--- Arduino/src/sketch.ino
+++ Arduino/src/sketch.ino
@@ -7,6 +7,8 @@ const unsigned char DISTANCE_TRIG_PIN = 2;
 
 const unsigned char DISTANCE_ECHO_PIN = 4;
 
+const unsigned char DISTANCE_SAMPLING_NUM = 3;
+
 enum Command { START, REQUEST_DISTANCE, SET_MOTORS };
 
 unsigned char received_command = 0;
@@ -17,38 +19,32 @@ long microsecondsToCentimeters(long duration){
 
 long read_distance(){
 	long duration;
+	long sum = 0;
 
-	digitalWrite(DISTANCE_TRIG_PIN, LOW);
-	delayMicroseconds(2);
-	digitalWrite(DISTANCE_TRIG_PIN, HIGH);
-	delayMicroseconds(10);
+	for(int i = 0; i < DISTANCE_SAMPLING_NUM; i++){
+		digitalWrite(DISTANCE_TRIG_PIN, LOW);
+		delayMicroseconds(2);
+		digitalWrite(DISTANCE_TRIG_PIN, HIGH);
+		delayMicroseconds(10);
 
-	digitalWrite(DISTANCE_TRIG_PIN, LOW);
+		digitalWrite(DISTANCE_TRIG_PIN, LOW);
 
-	duration = pulseIn(DISTANCE_ECHO_PIN, HIGH);
-
-	return microsecondsToCentimeters(duration);
+		duration = pulseIn(DISTANCE_ECHO_PIN, HIGH);
+		sum+=microsecondsToCentimeters(duration);
+		delay(35);
+	}
+	return sum/DISTANCE_SAMPLING_NUM;
+	//return microsecondsToCentimeters(duration);
 }
 
 
 void set_motor_powers(signed char left_power, signed char right_power){
 	if (left_power < -100 || left_power > 100 || right_power < -100 || right_power > 100) return;
 
-//	Serial.println(left_power);
-//	Serial.println(right_power);
-
 	int lp = 256*left_power/100;
 	int rp = 256*right_power/100;
 
-//	Serial.println(lp);
-//	Serial.println(rp);
-
 	int powers[] = {lp, 0, rp, 0};
-	
-//	Serial.println(powers[0]);
-//	Serial.println(powers[1]);
-//	Serial.println(powers[2]);
-//	Serial.println(powers[3]);
 
 	if (left_power < 0){
 		powers[0] = 0;
@@ -59,7 +55,6 @@ void set_motor_powers(signed char left_power, signed char right_power){
 		powers[3] = -rp;
 	}
 	for(unsigned char i = 0; i < sizeof(MOTOR_PINS)/sizeof(unsigned char); i++){
-//		Serial.println(powers[i]);
 		analogWrite(MOTOR_PINS[i], powers[i]);
 	}
 }
@@ -78,18 +73,7 @@ void setup() {
 	pinMode(DISTANCE_ECHO_PIN, INPUT);
 
 	Serial.begin(9600); // set the baud rate
-	//Serial.println("Ready"); // print "Ready" once
 
-/*	analogWrite(MOTOR_PINS[0], 200);
-	analogWrite(MOTOR_PINS[1], 0);
-	analogWrite(MOTOR_PINS[2], 200);
-	analogWrite(MOTOR_PINS[3], 0);
-
-	delay(1000);
-
-	analogWrite(MOTOR_PINS[0], 0);
-	analogWrite(MOTOR_PINS[2], 0);
-*/
 	while(Serial.available()==0);
 	received_command = Serial.read();
 	if(received_command == START){
@@ -102,18 +86,14 @@ void loop() {
 		received_command = Serial.read(); // read the incoming data
 		switch(received_command){
 			case REQUEST_DISTANCE: {
-				//Serial.println("DISTANCE REQUESTED");
 				Serial.println(read_distance());
 		 		break;
 				}
 			case SET_MOTORS: {
-		       		//Serial.println("SETTING MOTOR POWERS");
 				signed char left_power, right_power;
 				left_power = Serial.read() - 100;	//return to the original range [-100%,100%] 
 				right_power = Serial.read() - 100;
 				set_motor_powers(left_power, right_power);
-		//		Serial.println(left_power);
-		//		Serial.println(right_power);
 				break;
 				 }
 			default: {
