01f86384450d1f59aa6a4c1243ea43e4658b893c
Stojadin2701 <stojadin2701@gmail.com>
2017-05-24 21:15:35 +0000

    Ajax test

--- Pi/web/myscript.js
+++ Pi/web/myscript.js
@@ -1,4 +1,41 @@
 
+function xml_http_post(url, data, callback) {
+	var req = false;
+	try {
+		// Firefox, Opera 8.0+, Safari
+		req = new XMLHttpRequest();
+	}
+	catch (e) {
+		// Internet Explorer
+		try {
+			req = new ActiveXObject("Msxml2.XMLHTTP");
+		}
+		catch (e) {
+			try {
+				req = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+			catch (e) {
+				alert("Your browser does not support AJAX!");
+				return false;
+			}
+		}
+	}
+	req.open("POST", url, true);
+	req.onreadystatechange = function() {
+		if (req.readyState == 4) {
+			callback(req);
+		}
+	}
+	req.send(data);
+}
+
+function test_handle(req) {
+	var elem = document.getElementById('pose')
+		elem.innerHTML =  req.responseText
+}
+
+
+
 Myo.connect('test.test.test');
 
 Myo.on('connected', function(data, timestamp){
@@ -9,6 +46,8 @@ Myo.on('connected', function(data, timestamp){
 //Whenever we get a pose event, we'll update the image sources with the active version of the image
 Myo.on('pose', function(pose){
 	console.log(pose);
+	xml_http_post("index.html", pose, test_handle)
+
 })
 
 //Opposite of above. We also revert the main img to the unlocked state
